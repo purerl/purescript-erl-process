6d0972e3d01cb238c9daecf55ee6d87382361bc5
Rob "Hurricane" Ashton <robashton@users.noreply.github.com>
2019-05-23 14:11:38 +0000

    spawnLink + receiveAfter (#3)
    * spawnLink + receiveAfter
    
    * Pedantic whitespace
    
    Pedantic whitespace

--- src/Erl/Process/Raw.erl
+++ src/Erl/Process/Raw.erl
@@ -1,9 +1,17 @@
 -module(erl_process_raw@foreign).
--export([eqNative/2, spawn/1, send/1, 'receive'/0]).
+-export([
+         eqNative/2,
+         spawn/1,
+         spawnLink/1,
+         send/1,
+         'receive'/0,
+         receiveWithTimeout/2
+        ]).
 
 eqNative(X, Y) -> X == Y.
 
 spawn(F) -> fun () -> erlang:spawn(fun () -> F() end) end.
+spawnLink(F) -> fun () -> erlang:spawn_link(fun () -> F() end) end.
 
 send(Pid) -> fun (X) ->
   fun () ->
@@ -14,4 +22,13 @@ end.
 'receive'() ->
   fun () ->
     receive X -> X end
+  end.
+
+receiveWithTimeout(Timeout, Msg) ->
+  fun () ->
+    receive
+      X -> X
+    after
+      Timeout -> Msg
+    end
   end.
